82bce729aa2d76a57f2ce05e3a5a539891820411
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-01 00:54:10 +0545

    route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { BrowserRouter , Route , Switch } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Posts from './pages/posts';
+import Users from './pages/users';
 
 function App() {
   return (
-    <div >
-      <h1>Welcome to the main file</h1>
-    </div>
+    <BrowserRouter>
+    <Navbar />
+    <Switch>
+      <Route exact path ="/posts" component={Posts} />
+      <Route exact path ="/users" component={Users} />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
