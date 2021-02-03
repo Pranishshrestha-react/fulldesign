27e3dd90646390fd9d4c8c07db660c029a564b85
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-03 10:47:41 +0545

    post almost done

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,49 +1,52 @@
 import React from 'react';
+import axios from 'axios';
 import {
-    Card, CardImg, CardText, CardBody,
+    Card, Spinner, CardText, CardBody,
     CardTitle, CardSubtitle, Button, CardFooter
   } from 'reactstrap';
+ 
+
+const BASE_URL = "https://jsonplaceholder.typicode.com"
 
 class Posts extends React.Component{
     
     state ={
-        posts: [
-            {
-                "userId": 1,
-                "id": 1,
-                "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-                "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-              },
-              {
-                "userId": 1,
-                "id": 2,
-                "title": "qui est esse",
-                "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-              },
-              {
-                "userId": 1,
-                "id": 3,
-                "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-                "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-              },
-
-        ]
+        posts: [],
+        isLoading : false
 
     }
 
+    componentDidMount = () => {
+        this.setState({
+            ...this.state,
+            isLoading:true
+        })
 
+        axios.get(`${BASE_URL}/posts`).then(response =>{
+            this.setState({
+                posts: response.data,
+                isLoading:false
+            })
+            }).catch(error => {
+                this.setState({
+                    ...this.state,
+                    isLoading: false
+                })
+            })
+        }
 
+      
     render() {
+        if(this.state.isLoading) return <Spinner style={{margin:"20% 0 0 50%"}} type="grow" color="danger" />
         return <div className="container">
             {
                 this.state.posts.map(post => {
                     return (
-                        <Card className="mt-3">
-                            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                        <Card key={post.id} className="mt-3">
                             <CardBody>
                             <CardTitle tag="h5">{post.title}</CardTitle>
                             <CardText>{post.body}</CardText>
-                            <Button>Button</Button>
+                            
                             </CardBody>
                             <CardFooter>
                             <CardSubtitle tag="h6" className="mb-2 text-muted">Created By {post.userId}</CardSubtitle>
