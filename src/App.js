44da0ce7483ba11bc2db2e705598d83b6a39cb53
Pranishshrestha-react <respectsweetleaf@gmail.com>
2021-02-03 10:46:31 +0545

    user profile build in

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Posts from './pages/posts';
 import Users from './pages/users';
+import User from './pages/users/user.js';
+import Albums from './pages/album';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Switch>
       <Route exact path ="/posts" component={Posts} />
       <Route exact path ="/users" component={Users} />
+      <Route exact path ="/users/:id" component={User} />
+      <Route exact path ="/albums" component={Albums} />
     </Switch>
     </BrowserRouter>
   );
